a37e839f11120e011047d3a27f5426494aaef935
elafashionmkt <criativododo@gmail.com>
2026-02-02 08:10:23 -0300

    Add files via upload

---
+++ data/config-default.js
@@ -0,0 +1,17 @@
+/* config padrão do site (editável via painel, sem mudar stack) */
+
+window.ELA_CONFIG_DEFAULT = {
+  site: {
+    navClientLabel: "área do cliente"
+  },
+  clients: [
+    {
+      id: "jescri",
+      label: "jescri",
+      instagram: "@jescri",
+      password: "jescri",
+      redirect: "/clientes/jescri/",
+      logoPath: "/clientes/jescri/assets/logo-jescri.svg"
+    }
+  ]
+};
